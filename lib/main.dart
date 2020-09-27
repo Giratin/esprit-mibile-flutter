d04e606f11c11b7bbff291ea3a3cc7500c3ff725
Imed Amri <imedamri@MacBook-Air-de-Mohamed.local>
2020-09-27 20:41:56 +0100

    Flutter UI Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:workshop_flutter/car_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My First Flutter App",
+      title: "Cars Agency",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello World"),
+          title: Text("Cars Agency"),
         ),
-        body: Text("Hello World !!"),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              CarView("BMW", "Serie 3", "assets/bmw-serie3.jpg"),
+              CarView("Chery", "Tiggo 7", "assets/chery-tiggo7.jpg"),
+              CarView("KIA", "Sportage", "assets/kia-sportage.jpg"),
+              CarView("PEUGEOT", "208", "assets/peugeot-208.jpg"),
+            ],
+          ),
+        ),
       ),
     );
   }
